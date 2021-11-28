fe58601685a8cd910554a03be5fd9df041b636f6
dhaval9911 <dhaval8376@gmail.com>
2021-11-28 14:26:16 +0530

    updates

--- dcos.sh
+++ dcos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-    bash setup-0.sh
-    arch-chroot /mnt /root/dcos/setup-1.sh
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/dcos/setup-2.sh
+   bash setup-0.sh
+   arch-chroot /mnt /root/dcos/setup-1.sh
+   arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/dcos/setup-2.sh
 #    arch-chroot /mnt /root/ArchTitus/3-post-setup.sh
