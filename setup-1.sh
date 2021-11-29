264c63fdc8d5c14af02b03354ebe7a861a945735
dhaval9911 <dhaval8376@gmail.com>
2021-11-29 23:24:02 +0530

    updates

--- setup-1.sh
+++ setup-1.sh
@@ -244,18 +244,12 @@ fi
 
 echo -e "\nDone!\n"
 read -p "Please enter username:" username
-echo "username=$username" >> ${HOME}/dcos/install.conf
-if [ $(whoami) = "root"  ];
-then
-    useradd -m -G wheel -s /bin/bash $username 
-	passwd $username
-	cp -R /root/dcos /home/$username/
-    chown -R $username: /home/$username/dcos
-	read -p "Please name your machine:" nameofmachine
-	echo $nameofmachine > /etc/hostname
-else
-	echo "You are already a user proceed with aur installs"
-fi
+useradd -m -G wheel -s /bin/bash $username 
+passwd $username
+cp -R /root/dcos /home/$username/
+ chown -R $username: /home/$username/dcos
+read -p "Please name your machine:" nameofmachine
+echo $nameofmachine > /etc/hostname
 
 
 echo -e "\nINSTALLING AUR SOFTWARE\n"
