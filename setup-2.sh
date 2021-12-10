c8adf11c00af1e994969095ab681edbf88db3a46
Dhaval Chavda <71181356+dhaval9911@users.noreply.github.com>
2021-12-10 15:49:16 +0530

    Update setup-2.sh

--- setup-2.sh
+++ setup-2.sh
@@ -3,14 +3,14 @@
 echo -e "\nINSTALLING AUR SOFTWARE\n"
 # You can solve users running this script as root with this and then doing the same for the next for statement. 
 # However I will leave this up to you.
-sudo pacman -U /$username/dcos/yay.pkg.tar.zst --noconfirm 
-# echo "CLONING: YAY"
-# cd ~
-# git clone "https://aur.archlinux.org/yay.git"
-# cd ${HOME}/yay
-# makepkg -si --noconfirm
-# cd ~
-# touch "$HOME/.cache/zshhistory"
+#sudo pacman -U /$username/dcos/yay.pkg.tar.zst --noconfirm 
+ echo "CLONING: YAY"
+ cd ~
+ git clone "https://aur.archlinux.org/yay.git"
+ cd ${HOME}/yay
+ makepkg -si --noconfirm
+ cd ~
+ touch "$HOME/.cache/zshhistory"
 
 echo -e "\nINSTALLING AUR SOFTWARE\n"
 PKGS=(
@@ -44,12 +44,12 @@ cp -r /root/dcos/dotfiles/* $HOME/.config/
 cp -r /root/dcos/.zshrc $HOME/
 cp -r /root/dcos/dotfiles/* $HOME/.config/
 
-
+cd ~
 echo -e "Adding BlackArch Repo To The system "
 curl -O https://blackarch.org/strap.sh
 chmod +x strap.sh
 sudo ./strap.sh
-
+cd $HOME/dcos/
 #installing desktop environment
 
 function desktop() {
